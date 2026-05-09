50683c95972c24e6a89c15ee34e578f8a2b1b47d
akidesuu <ajboy.pelias123@gmail.com>
2026-05-09 21:57:20 +0800

    Fix

--- ph_recipe_backend/settings.py
+++ ph_recipe_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mn*u1jl_=+@fl)t^dd&ci-t4sp32iy88etep=83ie35y@of3&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.37', '127.0.0.1', 'localhost']
 
 
 # Application definition
