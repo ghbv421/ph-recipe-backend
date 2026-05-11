7a4aeb32b5fa0a72f82b2433582d8614ad61e244
akidesuu <ajboy.pelias123@gmail.com>
2026-05-12 00:08:30 +0800

    Fix

--- ph_recipe_backend/settings.py
+++ ph_recipe_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-mn*u1jl_=+@fl)t^dd&ci-t4sp32iy88etep=83ie35y@of3&k
 DEBUG = True
 
 # Combined both local and remote allowed hosts
-ALLOWED_HOSTS = ['192.168.1.35', '10.0.2.2', 'localhost', '127.0.0.1', '10.232.207.146']
+ALLOWED_HOSTS = ['192.168.1.37', '10.0.2.2', 'localhost', '127.0.0.1', '10.232.207.146']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
